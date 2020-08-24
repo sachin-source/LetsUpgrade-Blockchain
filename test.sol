8403d2fa63546fccfecb4f2965bdb131107513bd
sachin-source <sachinmgvt@gmail.com>
2020-08-24 22:05:31 +0530

    added solidity code

--- test.sol
+++ test.sol
@@ -1,29 +1,26 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity 0.4.22 <= 0.7.0;
 
 
 contract ElectricityBills {
-    string Name;
-    string addrs;
-    int units;
-    int money;
-    string consumerId;
-    
-    
-    
-    function ElectricityBills (string newName, string newAddrs, int newUnits, int newMoney, string newConsumerId) public {
-
-        Name = newName;
-        addrs = newAddrs;
-        units = newUnits;
-        money = newMoney;
-        consumerId = newConsumerId;
-
-    }
+ string Name;
+ string addrs;
+ int    units;
+ int    money;
+ string costomerId;
+ 
+ 
+ function ElectricityBills(string newName, string newAddrs, int newUnits, int newMoney, string newCustomerId) public {
+     Name = newName;
+     addrs = newAddrs;
+     units = newUnits;
+     money = newMoney;
+     costomerId = newCustomerId;
+ }
     
     function getElectricityBill () public view returns (string, string, int, int, string) {
         
-        return(Name, addrs, units, money, consumerId);
+        return(Name, addrs, units, money, costomerId);
         
     }
-    
 }
+
